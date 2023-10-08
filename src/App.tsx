1f5b197e990f215188a1899c5652fc389954d31e
Lucas <lucasclaudinocc@gmail.com>
2023-10-08 11:06:43 -0300

    Added base structure

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function CounterList() {
+  const [counters, setCounters] = useState(Array(96).fill(0));
+
+  // Function to handle incrementing a counter
+  const handleIncrement = (index) => {
+    const newCounters = [...counters];
+    newCounters[index] += 1;
+    setCounters(newCounters);
+  };
+
+  // Function to format time based on index
+  const formatTime = (index) => {
+    const hours = Math.floor(index * 15 / 60);
+    const minutes = (index * 15) % 60;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+  };
+
+  return (
+    <div>
+      <h1 className="w-[100svw] text-center pt-2 pb-5">Contador de Bicicletas</h1>
+      <ul className="grid grid-cols-3 text-center w-[100svw] gap-10">
+        {counters.map((count, index) => (
+          <li key={index}>
+            <h3>{formatTime(index)}</h3>
+            <p>Contagem: {count}</p>
+            <button onClick={() => handleIncrement(index)}>Acrescentar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CounterList;
